b747e1065701274342adf169782e6d7438988afa
Thomas Klepzig <thomas.klepzig@comparex.de>
2016-08-18 15:59:15 +0200

    added tests and manual stubs and mock

--- Projects/Mocking_Stubbing/Mocking_Stubbing/Service.cs
+++ Projects/Mocking_Stubbing/Mocking_Stubbing/Service.cs
@@ -1,4 +1,9 @@
-﻿namespace Mocking_Stubbing
+﻿using System.Collections.Generic;
+using System.Linq;
+using Shouldly;
+using Xunit;
+
+namespace Mocking_Stubbing
 {
     public class Service
     {
@@ -24,6 +29,78 @@
             if (customer.IsGoldCustomer)
                 return 90;
             return 0;
+        }
+    }
+
+
+    class CustomerRepositoryGold : ICustomerRepository
+    {
+        public void Save(Customer customer)
+        {
+        }
+
+        public Customer GetById(int id)
+        {
+            return new Customer() { IsGoldCustomer = true };
+        }
+    }
+
+    class CustomerRepositoryNonGold : ICustomerRepository
+    {
+        public void Save(Customer customer)
+        {
+        }
+
+        public Customer GetById(int id)
+        {
+            return new Customer() { IsGoldCustomer = false };
+        }
+    }
+
+    class CustomerRepositorySaveCustomerCalled : ICustomerRepository
+    {
+        public List<Customer> SavedCustomers { get; set; } = new List<Customer>();
+        public void Save(Customer customer)
+        {
+            SavedCustomers.Add(customer);
+        }
+
+        public Customer GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class ServiceTests
+    {
+        [Fact]
+        public void GetCustomerDiscountShouldReturnNinetyForGoldCustomer()
+        {
+            var customerRepositoryGold = new CustomerRepositoryGold();
+            var service = new Service(customerRepositoryGold);
+
+            service.GetCustomerDiscount(0).ShouldBe(90);
+        }
+
+        [Fact]
+        public void GetCustomerDiscountShouldReturnZeroForNonGoldCustomer()
+        {
+            var customerRepositoryNonGold = new CustomerRepositoryNonGold();
+            var service = new Service(customerRepositoryNonGold);
+
+            service.GetCustomerDiscount(0).ShouldBe(0);
+        }
+
+        [Fact]
+        public void AddCustomerShouldCallSave()
+        {
+            var repositorySaveCustomerCalled = new CustomerRepositorySaveCustomerCalled();
+            var service = new Service(repositorySaveCustomerCalled);
+
+            var customer = new Customer();
+            service.AddCustomer(customer);
+            repositorySaveCustomerCalled.SavedCustomers.Count.ShouldBe(1);
+            repositorySaveCustomerCalled.SavedCustomers.First().ShouldBe(customer);
         }
     }
 }

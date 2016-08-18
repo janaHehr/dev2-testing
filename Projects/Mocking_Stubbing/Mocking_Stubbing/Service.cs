997e239aa1a55b761671c66f1bb53ea565fc3d64
Alexander Tank <Alexander.Tank@comparex.de>
2016-08-18 15:50:02 +0200

    Manuelles Stubbing

--- Projects/Mocking_Stubbing/Mocking_Stubbing/Service.cs
+++ Projects/Mocking_Stubbing/Mocking_Stubbing/Service.cs
@@ -1,4 +1,8 @@
-﻿namespace Mocking_Stubbing
+﻿using NSubstitute;
+using Shouldly;
+using Xunit;
+
+namespace Mocking_Stubbing
 {
     public class Service
     {
@@ -24,6 +28,50 @@
             if (customer.IsGoldCustomer)
                 return 90;
             return 0;
+        }
+    }
+
+
+    public class CustomerRepositoryStub : ICustomerRepository
+    {
+        public bool IsGoldCustomer { get; set; }
+
+        public void Save(Customer customer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Customer GetById(int id)
+        {
+            return new Customer {IsGoldCustomer = IsGoldCustomer };
+        }
+    }
+
+    public class ServiceTests
+    {
+        private readonly CustomerRepositoryStub _stub;
+        private readonly Service _sut;
+
+        public ServiceTests()
+        {
+            _stub = new CustomerRepositoryStub();
+            _sut = new Service(_stub);
+        }
+
+        [Fact]
+        public void Should_return_discount_of_90_for_gold_customer()
+        {
+            _stub.IsGoldCustomer = true;
+            var customerDiscount = _sut.GetCustomerDiscount(0);
+            customerDiscount.ShouldBe(90);
+        }
+
+        [Fact]
+        public void Should_return_discount_of_0_for_none_gold_customer()
+        {
+            _stub.IsGoldCustomer = false;
+            var customerDiscount = _sut.GetCustomerDiscount(0);
+            customerDiscount.ShouldBe(0);
         }
     }
 }
